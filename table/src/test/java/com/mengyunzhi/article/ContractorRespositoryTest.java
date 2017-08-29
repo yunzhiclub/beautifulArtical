766f02fec22a3d1d43da1fd6ff244ffa57b74162
chaoweb <1648907448@qq.com>
2017-08-29 20:58:33 +0800

    修改断言

--- table/src/test/java/com/mengyunzhi/article/ContractorRespositoryTest.java
+++ table/src/test/java/com/mengyunzhi/article/ContractorRespositoryTest.java
@@ -26,8 +26,9 @@ public class ContractorRespositoryTest {
         contractor.setFax("654846345");
         contractor.setMobilePhone("57468435435");
         contractor.setEmail("zhangyoushan@yunzhi.com");
+        contractorRespository.save(contractor);
         
-        assertThat(contractorRespository.save(contractor)).isNotNull();
+        assertThat(contractor.getId()).isNotNull();
     }
 
 }
