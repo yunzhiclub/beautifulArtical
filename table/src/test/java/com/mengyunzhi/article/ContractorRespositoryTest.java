58417a94fc952c3ec44dd2080c7399ea1234a4b5
chaoweb <1648907448@qq.com>
2017-08-29 20:28:06 +0800

    测试增加断言

--- table/src/test/java/com/mengyunzhi/article/ContractorRespositoryTest.java
+++ table/src/test/java/com/mengyunzhi/article/ContractorRespositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by Poshichao on 17/8/29.
  */
@@ -18,8 +20,14 @@ public class ContractorRespositoryTest {
 
     @Test
     public void save() {
-        contractorRespository.save(new Contractor());
-
+        Contractor contractor = new Contractor();
+        contractor.setName("张友善");
+        contractor.setPhone("1225458878");
+        contractor.setFax("654846345");
+        contractor.setMobilePhone("57468435435");
+        contractor.setEmail("zhangyoushan@yunzhi.com");
+        
+        assertThat(contractorRespository.save(contractor)).isNotNull();
     }
 
 }
