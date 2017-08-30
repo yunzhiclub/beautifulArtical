d7fe705add9778f69364db397943970c9fed0c8b
zhuchenshu <1369359429@qq.com>
2017-08-30 10:54:38 +0800

    将Sortable插件的配置文件放在单独的js文件中，加注释

--- thinkphp/application/index/controller/ArticleController.php
+++ thinkphp/application/index/controller/ArticleController.php
@@ -2,16 +2,18 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Request;
 
 /**
  * 
- * @authors Your Name (you@example.org)
+ * @authors 朱晨澍
  * @date    2017-08-30 09:08:35
  * @version $Id$
  */
 
 class ArticleController extends Controller {
     public function add(){
+
     	return $this->fetch();
     }
 }
