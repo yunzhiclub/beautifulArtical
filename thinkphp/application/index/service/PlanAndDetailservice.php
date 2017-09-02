26e53fd9833ff5129bf333ade363592b75bcab82
chenzhigao <2934472815@qq.com>
2017-09-03 03:45:49 +0800

    方案报价重构

---
+++ thinkphp/application/index/service/PlanAndDetailservice.php
@@ -0,0 +1,87 @@
+<?php
+namespace app\index\service;
+
+use app\index\model\Common;
+use app\index\model\Detail;
+use app\index\model\Plan;
+use app\index\model\Article;
+use think\Request;
+
+
+class PlanAndDetailservice
+{
+	public function addPlanAndDetail($param)
+	{
+		//初始化返回信息
+        $message = [];
+        $message['status'] = 'success';
+        $message['route'] = 'Article/secondadd';
+        $message['message'] = '添加成功，请继续完善文章详情';
+        
+        //获取到参数
+        $Article = new Article();
+		$articleId = Request::instance()->param('id');
+		$planId = $param->param('id/d');
+		$travelDate = $param->post('travelDate');
+        $peopleNum = $param->post('peopleNum');
+        $currency = $currency = $param->post('currency');
+        $totalCost = $param->post('totalCost');
+        $lastPayTime = $param->post('lastPayTime');
+        $dijie_remark = $param->post('dijie_remark');
+        $dijie_number = $param->post('dijie_number');
+        $dijie_frequency = $param->post('dijie_frequency');
+        $dijie_unitPrice = $param->post('dijie_unitPrice');
+        $dijie_totalPrice = $param->post('dijie_totalPrice');
+        $zhusu_remark = $param->post('zhusu_remark');
+        $zhusu_number = $param->post('zhusu_number');
+        $zhusu_frequency = $param->post('zhusu_frequency');
+        $zhusu_unitPrice = $param->post('zhusu_unitPrice');
+        $zhusu_totalPrice = $param->post('zhusu_totalPrice');
+
+        $message['param']['id'] = $articleId;
+
+        // 实例化一个空的方案报价
+        $Plan = new Plan();
+        $Plan->article_id = $articleId;
+		$Plan->travel_date = $travelDate;
+        $Plan->people_num = $peopleNum;
+        $Plan->currency = $currency;
+        $Plan->total_cost = $totalCost;
+        $Plan->last_pay_time = $lastPayTime;
+
+        // 添加数据
+        if (!$Plan->save()) {
+            return $this->error('数据添加错误：' . $Plan->getError());
+        }
+
+
+        // 实例化一个空的明细信息
+		$Detail = new Detail();
+		$Detail->remark = $dijie_remark;
+		$Detail->plan_id = $Plan->id;
+		$Detail->type = 'dijie';
+		$Detail->number = $dijie_number;
+		$Detail->frequency = $dijie_frequency;
+		$Detail->unit_price = $dijie_unitPrice;
+		$Detail->total_price = $dijie_totalPrice;
+		// 添加数据
+        if (!$Detail->save()) {
+            return $this->error('数据添加错误：' . $Detail->getError());
+        }
+        // 实例化另一个空的明细信息
+        $Detail1 = new Detail();
+		$Detail1->remark = $zhusu_remark;
+		$Detail1->plan_id = $Plan->id;
+		$Detail1->type = 'zhusu';
+		$Detail1->number = $zhusu_number;
+		$Detail1->frequency = $zhusu_frequency;
+		$Detail1->unit_price = $zhusu_unitPrice;
+		$Detail1->total_price = $zhusu_totalPrice;
+		// 添加数据
+        if (!$Detail1->save()) {
+            return $this->error('数据添加错误：' . $Detail->getError());
+        }
+        return $message;
+
+	}
+}
