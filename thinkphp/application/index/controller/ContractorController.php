53a1117b7eed57df897d34dbd3d3154bd2fe7e11
chaoweb <1648907448@qq.com>
2017-09-03 15:23:59 +0800

    重构订制师，添加后返回article_id

--- thinkphp/application/index/controller/ContractorController.php
+++ thinkphp/application/index/controller/ContractorController.php
@@ -2,28 +2,53 @@
 namespace app\index\controller;
 
 use think\Controller;
-use app\index\model\Contractor;
 use think\Request;
+use app\index\model\Contractor;
+use app\index\service\Contractorservice;
 
 /**
  * @author 朴世超
  */
 class ContractorController extends Controller
 {
+	protected $contractorService = null;
+
+	// 构造函数实例化Contractorservice
+	function __construct(Request $requets = null)
+	{
+		parent::__construct($requets);
+		// 实例化服务层
+		$this->contractorService = new Contractorservice();
+	}
+
 	public function add()
 	{
+		// 获取文章id
+		$articleId = Request::instance()->param('article_id');
+
+		// 连接v层
+		$this->assign('article_id', $articleId);
+
+		// 返回添加界面
 		return $this->fetch();
 	}
+
 	public function save()
 	{
-		// 接收表单数据
-		$postData = Request::instance()->post();
-		$article_id = Request::instance()->param('id');
-		$Contractor = new Contractor();
-		if ($Contractor->saveContractor($postData, $article_id)) {
-			return $this->success('保存成功！', url('article/secondadd'));
-		}
+		// 接收数据
+		$param = Request::instance();
 
-		return $this->error('保存失败！');
+		// 调用Service层保存方法
+		$message = $this->contractorService->saveOrUpdateContractor($param);
+
+		// 返回相应的界面
+		if ($message['status'] === 'success') {
+			// 返回保存成功界面
+			return $this->success($message['message'], url($message['route'], ['id'=>$message['id']]));
+
+		} else {
+			// 返回保存失败界面
+			return $this->error($message['message'], url($message['route'], ['id'=>$message['id']]));
+		}
 	}
 }
