06ef9174dde356cf56405591c59b75a49d248ce7
zhangxishuo <zhangxishuo1998@gmail.com>
2017-09-04 16:09:11 +0800

    方案报价获取完成

--- thinkphp/application/index/model/Plan.php
+++ thinkphp/application/index/model/Plan.php
@@ -9,5 +9,12 @@ use think\Model;
  */
 class Plan extends Model
 {
+    public function getPlanByArticleId($id) {
+        return $this->where('article_id','=',$id)->select();
+    }
 
+    public function getDetailByType($type) {
+        $map = ['type' => $type, 'plan_id' => $this->id];
+        return Detail::get($map);
+    }
 }
