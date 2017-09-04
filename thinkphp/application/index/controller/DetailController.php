3e99b89b30cacfe9dbc5fb7c6f467ca751dfabcc
chenzhigao <2934472815@qq.com>
2017-09-04 16:34:58 +0800

    一个service改为两个service

--- thinkphp/application/index/controller/DetailController.php
+++ thinkphp/application/index/controller/DetailController.php
@@ -2,36 +2,22 @@
 namespace app\index\controller;
 use think\Request;
 use think\Controller;
-use app\index\model\Detail;
-use app\index\model\Plan;
-use app\index\model\Article;
-use app\index\service\PlanAndDetailservice;
+use app\index\service\PlanService;
+use app\index\service\DetailService;
 
 
 class DetailController extends Controller
 {
-
-	protected $planAndDetailService = null;
-
-    //构造函数实例化ArticleService
     function __construct(Request $request = null)
     {
         parent::__construct($request);
-        //实例化服务层
-        $this->planAndDetailService = new PlanAndDetailservice();
+        $this->planService = new PlanService();
+        $this->detailService = new DetailService();
     }
-
-    public function index()
-	{
-		$PageSize = config('paginate.var_page');
-	    $details = Detail::order('id desc')->paginate($PageSize);
-	    $this->assign('details', $details);
-		return $this->fetch();
-	}
 
 	public function add()
 	{
-		$articleId = Request::instance()->param('id');
+		$articleId = Request::instance()->param('id/d');
 		$this->assign('id', $articleId);
         return $this->fetch();
 	}
@@ -39,20 +25,16 @@ class DetailController extends Controller
     // add页面完成后触发事件
 	public function save()
 	{
-        $Article = new Article();
-        $articleId = Request::instance()->param('id');
 		//接受参数
         $param = Request::instance();
         //调用service中的保存方法
-        $message =  $this->planAndDetailService->addPlanAndDetail($param);
-
-        //返回相应的界面
-        if ($message['status'] === 'success') {
-            //跳转成功的界面
-            $this->success($message['message'], url($message['route'], ['id' => $message['articleId']]));
-        } else {
-            //跳转失败的界面
-            $this->error($message['message'], url($message['route']));
+        $Plan = $this->planService->save($param);
+        
+        if($this->detailService->add($param,$Plan)) {
+            return $this->success('添加成功，请继续完善文章详情', url('Article/secondadd'));
         }
+        
+        $this->error('数据添加错误：', url('add'));
+        
 	}
 }
