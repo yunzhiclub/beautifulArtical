04447be3841a2762d810ea5348ad6ed01a12e4cd
zhangxishuo <zhangxishuo1998@gmail.com>
2017-09-11 22:10:45 +0800

    查询功能实现

--- thinkphp/application/index/service/Materialservice.php
+++ thinkphp/application/index/service/Materialservice.php
@@ -178,4 +178,13 @@ class Materialservice  {
         }
         return $message;
     }
+
+    public function searchMaterial($materialName, $pageSize) {
+        $material = new Material();
+        if(!empty($materialName)) {
+            $material->where('designation', 'like', '%'. $materialName. '%');
+        }
+        $materials = $material->order('id desc')->paginate($pageSize);
+        return $materials;
+    }
 }
