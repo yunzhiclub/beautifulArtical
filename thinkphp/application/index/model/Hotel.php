57b4f33353bbe0674ff3197be21da015778ba840
zhangxishuo <zhangxishuo1998@gmail.com>
2017-09-12 20:12:24 +0800

    解决冲突

--- thinkphp/application/index/model/Hotel.php
+++ thinkphp/application/index/model/Hotel.php
@@ -11,40 +11,5 @@ use think\Model;
  */
 
 class Hotel extends Model {
-    public function saveHotel($name, $city, $starLevel, $remark) {
-        $this->designation = $name;
-        $this->city = $city;
-        $this->star_level = $starLevel;
-        $this->remark = $remark;
 
-        if(!$this->save()) {
-            return $this->getError();
-        } else {
-            return $this;
-        }
-    }
-
-    public function updateHotel($name, $city, $starLevel, $remark, $id) {
-        $hotel = Hotel::get($id);
-        return $hotel->saveHotel($name, $city, $starLevel, $remark);
-    }
-
-    public function deleteHotel($id) {
-        $hotel = Hotel::get($id);
-        if(!$hotel->delete()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public static function getNullHotel() {
-        $hotel = new Hotel();
-        $hotel->id = null;
-        $hotel->designation = '';
-        $hotel->city = '';
-        $hotel->star_level = '';
-        $hotel->remark = '';
-        return $hotel;
-    }
 }
