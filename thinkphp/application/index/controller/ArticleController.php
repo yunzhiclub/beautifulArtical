983ebabe87593a901f274b83a5ad30ca3484d85b
chaoweb <1648907448@qq.com>
2017-09-16 19:56:52 +0800

    解决 方案报价为空显示

--- thinkphp/application/index/controller/ArticleController.php
+++ thinkphp/application/index/controller/ArticleController.php
@@ -99,15 +99,28 @@ class ArticleController extends IndexController {
         $articleId = $param->param('articleId');
         $Plan = new Plan();
         $Plans = $Plan->getPlanByArticleId($articleId);
-        // 使用number_format 格式化输入金额
-        $date = $this->articleService->MoneyFormate($Plans);
 
-        // 获取报价方案相同的字段并传入前台
-        $adultNum = $Plans[0]->adult_num;
-        $childNum = $Plans[0]->child_num;
-        $currency = $Plans[0]->currency;
-        $totalCost = $Plans[0]->total_cost;
-        $lastPayTime = $Plans[0]->last_pay_time;
+        // 报价方案为空
+        if (!empty($Plans)) {
+            // 使用number_format 格式化输入金额
+            $date = $this->articleService->MoneyFormate($Plans);
+
+            // 获取报价方案相同的字段
+            $adultNum = $Plans[0]->adult_num;
+            $childNum = $Plans[0]->child_num;
+            $currency = $Plans[0]->currency;
+            $totalCost = $Plans[0]->total_cost;
+            $lastPayTime = $Plans[0]->last_pay_time;
+
+        } else {
+            $adultNum = '';
+            $childNum = '';
+            $currency = '';
+            $totalCost = '';
+            $lastPayTime = '';
+        }
+        
+        // 方案报价字段传向前台
         $this->assign('adultNum', $adultNum);
         $this->assign('childNum', $childNum);
         $this->assign('currency', $currency);
