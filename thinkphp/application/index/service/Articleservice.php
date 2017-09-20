b01630acc5af2f0de4723d9814295546ea122425
chaoweb <1648907448@qq.com>
2017-09-20 11:33:09 +0800

    初步实现价钱显示格式化

--- thinkphp/application/index/service/Articleservice.php
+++ thinkphp/application/index/service/Articleservice.php
@@ -375,17 +375,20 @@ class Articleservice
             $TotalCost = $this->fromate($value->total_cost);
             $value->total_cost = $TotalCost;
 
-//            // 格式化成人单价
-//            $adultUnitPrice = $this->fromate($value->adult_unit_price);
-//            $value->adult_unit_price = $adultUnitPrice;
-//
-//            // 格式化儿童单价
-//            $childUnitPrice = $this->fromate($value->child_unit_price);
-//            $value->child_unit_price = $childUnitPrice;
-//
-//            // 格式化总价
-//            $totalPrice = $this->fromate($value->total_price);
-//            $value->total_price = $totalPrice;
+            // 根据方案报价获取所有detail
+            $planId = $value->id;
+            $Detail = new Detail();
+            $Details = $Detail->where('plan_id', $planId)->select();
+
+            // 遍历，格式化
+            foreach ($Details as $key => $value) {
+                $adultUnitPrice = $this->fromate($value->adult_unit_price);
+                $value->adult_unit_price = $adultUnitPrice;
+                $childUnitPrice = $this->fromate($value->child_unit_price);
+                $value->child_unit_price = $childUnitPrice;
+                $totalPrice = $this->fromate($value->total_price);
+                $value->total_price = $totalPrice;
+            }
         }
     }
     // 格式化金额函数
