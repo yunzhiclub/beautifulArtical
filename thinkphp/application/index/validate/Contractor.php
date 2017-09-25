023dfb83d8afbea47ee25605d7387e4b452a71b4
chenzhigao <2934472815@qq.com>
2017-09-25 21:10:42 +0800

    代码完善

--- thinkphp/application/index/validate/Contractor.php
+++ thinkphp/application/index/validate/Contractor.php
@@ -5,16 +5,16 @@ use think\Validate;
 class Contractor extends Validate
 {
 	protected $rule = [
-        'designation'  => 'require|length:2,5',
-        'mobile' => 'require|length:11',
+        'designation'  => 'require|length:2,30',
         'phone' => 'require|length:11',
+        'mobile' => 'require|length:11',
         'email' => 'email'
     ];
     
     protected $message = [
-        'name'  =>  '用户名e',
-        'mobile' => '手机号码长度不正确',
-        'phone' => '电话长度不正确',
+        'designation'  =>  '用户名介于2到30之间',
+        'phone' => '电话长度应为11',
+        'mobile' => '手机号码长度应为11',
         'email' =>  '邮箱格式不正确',
     ];
 }
