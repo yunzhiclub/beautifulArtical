adfcff20cad85fdeae93008a6b81b3a40cdb8c3c
zhangxishuo <zhangxishuo1998@Gmail.com>
2018-03-08 18:11:24 +0800

    '功能完善'

--- thinkphp/application/index/model/Plan.php
+++ thinkphp/application/index/model/Plan.php
@@ -29,31 +29,6 @@ class Plan extends Model
         return $details;
     }
 
-//    public function getDetail($type, $plan) {
-//        $Detail = new Detail();
-//
-//        if (empty($plan->id) || empty($Detail->where('plan_id', $plan->id)->select())) {
-//            // 不存在id，细节字段置空
-//            $Detail->adult_unit_price = '';
-//            $Detail->child_unit_price = '';
-//            $Detail->total_price = '';
-//            $Detail->remark = '';
-//
-//        }  else {
-//            // 根据id和db_type获取细节
-//            $Detail = $Detail->where('plan_id', $plan->id)->where('db_type', $type)->find();
-//            if (empty($Detail)) {
-//                $Detail = new Detail();
-//                $Detail->adult_unit_price = '';
-//                $Detail->child_unit_price = '';
-//                $Detail->total_price = '';
-//                $Detail->remark = '';
-//            }
-//        }
-//
-//        return $Detail;
-//    }
-
     /**
      * 克隆一个日程实体
      * @param  [type] $planId [description]
