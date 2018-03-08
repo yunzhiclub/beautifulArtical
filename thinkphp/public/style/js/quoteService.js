e445034a565b4bb2dcba3dd6b844619787a3f06a
zhangxishuo <zhangxishuo1998@Gmail.com>
2018-03-08 13:00:50 +0800

    '方案报价，前台显示功能完成'

---
+++ thinkphp/public/style/js/quoteService.js
@@ -0,0 +1,56 @@
+'use strict';
+
+$(function() {
+    /**
+     * 待插入的节点，拼接HTML
+     * 目前还没有找到能把这段HTML代码分离出来的方法
+     * @type {string}
+     */
+    var quote = '<div class="row">\n' +
+        '    <div class="form-group col-md-3">\n' +
+        '        <label>名称</label>\n' +
+        '        <input type="text" class="form-control">\n' +
+        '    </div>\n' +
+        '    <div class="form-group col-md-2">\n' +
+        '        <label>成人单价</label>\n' +
+        '        <input type="text" class="form-control">\n' +
+        '    </div>\n' +
+        '    <div class="form-group col-md-2">\n' +
+        '        <label>儿童单价</label>\n' +
+        '        <input type="text" class="form-control">\n' +
+        '    </div>\n' +
+        '    <div class="form-group col-md-2">\n' +
+        '        <label>总价</label>\n' +
+        '        <input type="text" class="form-control" readonly>\n' +
+        '    </div>\n' +
+        '    <div class="form-group col-md-3">\n' +
+        '        <label>备注</label>\n' +
+        '        <input type="text" class="form-control">\n' +
+        '    </div>\n' +
+        '</div>';
+    /**
+     * newQuote新建方案报价
+     * 为新建按钮添加点击事件
+     */
+    $('#newQuote').click(function() {
+        // 在方案报价组中追加元素
+        $('#quoteGroup').append(quote);
+    });
+    /**
+     * removeQuote删除方案报价
+     * 为删除按钮添加点击事件
+     */
+    $('#removeQuote').click(function() {
+        // 获取节点组中所有方案报价
+        var node = $('#quoteGroup>.row');
+        // 获取方案报价个数
+        var length = node.length;
+        // 如果方案报价不为空
+        if (length > 0) {
+            // 待删除节点设置为最后一个方案报价
+            var deleteNode = node[length - 1];
+            // 删除方案报价
+            deleteNode.remove();
+        }
+    });
+});
